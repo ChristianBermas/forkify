6b8ecaa2dcff6ec48429262039c4950c3a767b45
Christian Bermas <changianan588@outlook.com>
2026-01-16 13:24:20 +0900

    Initial Commit

---
+++ src/js/config.js
@@ -0,0 +1,6 @@
+// const res = await fetch(`https://forkify-api.jonas.io/api/v2/recipes/5ed6604591c37cdc054bc886`);
+export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '89310273-9bb2-4124-8a0f-dbd3ddd44fb7';
+export const MODAL_CLOSE_SEC = 2.5;
